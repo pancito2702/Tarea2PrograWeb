0b19c44186df5b2310885e20f5f4a8be6087d400
pancito2702 <creeper27022@gmail.com>
2022-10-03 14:34:25 -0600

    Tarea Terminada

---
+++ src/main/java/equipos/EquiposApplication.java
@@ -0,0 +1,13 @@
+package equipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/equipos/EquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
